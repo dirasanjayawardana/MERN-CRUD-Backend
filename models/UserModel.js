cb2a74676f71ad4732124c4ef078ea7f6a6815d3
dirasanjayawardana <dirawardana.dw@gmail.com>
2023-02-13 10:13:32 +0700

    update user model untuk cardedithvtdiv

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,15 +1,111 @@
+// import mongoose from "mongoose";
+
+// const User = mongoose.Schema({
+//     name:{
+//         type: String,
+//         required: true
+//     },
+//     email:{
+//         type: String,
+//         required: true
+//     },
+//     gender:{
+//         type: String,
+//         required: true
+//     }
+// });
+
+// export default mongoose.model('Users', User);
+
+
 import mongoose from "mongoose";
 
 const User = mongoose.Schema({
-    name:{
+    tshAll:{
         type: String,
         required: true
     },
-    email:{
+    tshGpm:{
         type: String,
         required: true
     },
-    gender:{
+    haCut:{
+        type: String,
+        required: true
+    },
+    haBalance:{
+        type: String,
+        required: true
+    },
+    sugarTch:{
+        type: String,
+        required: true
+    },
+    sugarRend:{
+        type: String,
+        required: true
+    },
+    sugarEst:{
+        type: String,
+        required: true
+    },
+    plantCap:{
+        type: String,
+        required: true
+    },
+    plantTotal:{
+        type: String,
+        required: true
+    },
+    gerRpc:{
+        type: String,
+        required: true
+    },
+    gerR1:{
+        type: String,
+        required: true
+    },
+    gerR2:{
+        type: String,
+        required: true
+    },
+    gerR3:{
+        type: String,
+        required: true
+    },
+    gerR4:{
+        type: String,
+        required: true
+    },
+    rfGlifosat:{
+        type: String,
+        required: true
+    },
+    rfPlough:{
+        type: String,
+        required: true
+    },
+    rfPlant:{
+        type: String,
+        required: true
+    },
+    rfPre:{
+        type: String,
+        required: true
+    },
+    rfFertilize:{
+        type: String,
+        required: true
+    },
+    weedsLow:{
+        type: String,
+        required: true
+    },
+    weedsMed:{
+        type: String,
+        required: true
+    },
+    weedsHigh:{
         type: String,
         required: true
     }
